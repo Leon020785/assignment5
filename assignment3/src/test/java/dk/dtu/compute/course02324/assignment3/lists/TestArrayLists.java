9efaf45051def3737c9f42c1abec0c54f83d8993
leon <leonlorenzen85@gmail.com>
2025-03-21 10:25:21 +0100

    starten på den ny opgave 5

--- assignment3/src/test/java/dk/dtu/compute/course02324/assignment3/lists/TestArrayLists.java
+++ assignment3/src/test/java/dk/dtu/compute/course02324/assignment3/lists/TestArrayLists.java
@@ -8,10 +8,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
-/**
- * This test class sets up ({@link #setUp()}}) and adds some more
- * tests for {@link SortedArrayList} based on {@link TestForAllLists}.
- */
+
 public class TestArrayLists extends TestForAllLists{
 
     @Before
